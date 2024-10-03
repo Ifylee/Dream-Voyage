07a5be0c708bd644c053bf593b891e143eabbb2d
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-10-02 17:42:44 -0700

    added all the images and updated the seed file to have the images names

--- server/seed/index.js
+++ server/seed/index.js
@@ -2,6 +2,12 @@ const db = require("../config/connection");
 const { User, Trip, Category } = require("../models");
 
 db.once("open", async () => {
+
+    await Trip.deleteMany({});
+    await User.deleteMany({});
+    await Category.deleteMany({});
+
+
   const categories = await Category.insertMany([
     { name: "Africa" },
     { name: "Asia" },
@@ -18,7 +24,7 @@ db.once("open", async () => {
       summary: "This is a test",
       description:
         "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-      img: "cookie-tin.jpg",
+      img: "lagos.jpg",
       category: categories[0]._id,
       price: 2.99,
     },
@@ -27,7 +33,7 @@ db.once("open", async () => {
       summary: "This is a test",
       description:
         "Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.",
-      img: "canned-coffee.jpg",
+      img: "zanzibar.jpg",
       category: categories[0]._id,
       price: 1.99,
     },
@@ -37,7 +43,7 @@ db.once("open", async () => {
       category: categories[1]._id,
       description:
         "Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.",
-      img: "toilet-paper.jpg",
+      img: "japan.jpg",
       price: 7.99,
     },
     {
@@ -46,7 +52,7 @@ db.once("open", async () => {
       category: categories[1]._id,
       description:
         "Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.",
-      img: "soap.jpg",
+      img: "seoul.jpg",
       price: 3.99,
     },
     {
@@ -55,7 +61,7 @@ db.once("open", async () => {
       summary: "This is a test",
       description:
         "Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.",
-      img: "wooden-spoons.jpg",
+      img: "singapore.jpg",
       price: 14.99,
     },
     {
@@ -64,7 +70,7 @@ db.once("open", async () => {
       category: categories[2]._id,
       description:
         "Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.",
-      img: "camera.jpg",
+      img: "vienna.jpg",
       price: 399.99,
     },
     {
@@ -73,7 +79,7 @@ db.once("open", async () => {
       category: categories[2]._id,
       description:
         "In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.",
-      img: "tablet.jpg",
+      img: "amesterdam.jpg",
       price: 199.99,
     },
     {
@@ -82,7 +88,7 @@ db.once("open", async () => {
       category: categories[3]._id,
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.",
-      img: "bedtime-book.jpg",
+      img: "shire.jpg",
       price: 9.99,
     },
     {
@@ -91,7 +97,7 @@ db.once("open", async () => {
       category: categories[4]._id,
       description:
         "Ut vulputate hendrerit nibh, a placerat elit cursus interdum.",
-      img: "spinning-top.jpg",
+      img: "mexico-city.jpg",
       price: 1.99,
     },
     {
@@ -100,7 +106,7 @@ db.once("open", async () => {
       category: categories[4]._id,
       description:
         "Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.",
-      img: "plastic-horses.jpg",
+      img: "machu-pichu.jpg",
       price: 2.99,
     },
     {
@@ -109,16 +115,16 @@ db.once("open", async () => {
       category: categories[4]._id,
       description:
         "Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.",
-      img: "teddy-bear.jpg",
+      img: "vancouver.jpg",
       price: 7.99,
     },
     {
-      title: "São Paulo Brazil",
+      title: "Rio de Janeiro Brazil",
       summary: "This is a test",
       category: categories[4]._id,
       description:
         "Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.",
-      img: "alphabet-blocks.jpg",
+      img: "brazil.jpg",
       price: 9.99,
     },
   ]);
