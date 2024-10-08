75cdc707e185c7c174a75e262ec7f546bf2dd2d8
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-10-08 08:43:25 -0700

    updated the reducer to remove cart item and the data that needs to be received from cart from _id to id so that it remove a single item from cart not everything

--- client/src/components/Cart/cart.jsx
+++ client/src/components/Cart/cart.jsx
@@ -1,83 +1,3 @@
-// import React from "react";
-// import Button from "@mui/material/Button";
-// import Dialog from "@mui/material/Dialog";
-// import ListItemText from "@mui/material/ListItemText";
-// import ListItemButton from "@mui/material/ListItemButton";
-// import List from "@mui/material/List";
-// import Divider from "@mui/material/Divider";
-// import AppBar from "@mui/material/AppBar";
-// import Toolbar from "@mui/material/Toolbar";
-// import IconButton from "@mui/material/IconButton";
-// import Typography from "@mui/material/Typography";
-// import CloseIcon from "@mui/icons-material/Close";
-// import Slide from "@mui/material/Slide";
-// import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-// import { Box } from "@mui/material";
-
-// const Transition = React.forwardRef(function Transition(props, ref) {
-//   return <Slide direction="down" ref={ref} {...props} />;
-// });
-// export const FullScreenDialog = () => {
-//   const [open, setOpen] = React.useState(false);
-//   const handleClickOpen = () => {
-//     setOpen(true);
-//   };
-//   const handleClose = () => {
-//     setOpen(false);
-//   };
-//   return (
-//     <React.Fragment>
-//       <Button
-//         variant=""
-//         startIcon={<ShoppingCartIcon fontSize="large" />}
-//         onClick={handleClickOpen}
-//       >
-//         <span role="img" aria-label="trash">
-//           {/* <ShoppingCartIcon fontSize="small" sx={{color:"white"}}/> */}
-//         </span>
-//       </Button>
-//       <Dialog
-//         fullScreen
-//         open={open}
-//         onClose={handleClose}
-//         TransitionComponent={Transition}
-//       >
-//         <AppBar  sx={{ position: "relative", backgroundColor: "black" }}>
-//           <Toolbar>
-//             <IconButton
-//               edge="start"
-//               color="inherit"
-//               onClick={handleClose}
-//               aria-label="close"
-//             >
-//               <CloseIcon />
-//             </IconButton>
-//             <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-//               Cart
-//             </Typography>
-//             <Button color="inherit" variant="outlined" onClick={handleClose}>
-//               Buy
-//             </Button>
-//           </Toolbar>
-//         </AppBar>
-//         <List>
-//           <ListItemButton>
-//             <ListItemText primary="Phone ringtone" secondary="Titania" />
-//             <Box ml="auto">
-//               <ListItemText primary="🗑️" onClick={handleClose} />
-//             </Box>
-//           </ListItemButton>
-//           <Divider />
-//           <ListItemButton>
-//             <ListItemText primary="Default notification ringtone" />
-//           </ListItemButton>
-//         </List>
-//       </Dialog>
-//     </React.Fragment>
-//   );
-// };
-
-
 
 import React from "react";
 import { useMutation } from "@apollo/client";
@@ -103,6 +23,7 @@ import RemoveIcon from '@mui/icons-material/Remove';
 import { Box, Divider, Paper, Badge, TextField } from "@mui/material";
 import { REMOVE_TRIP_FROM_CART } from "../../utils/actions";
 const stripePromise = loadStripe('your_stripe_publishable_key');
+
 export const FullScreenDialog = ({ icon }) => {
   const [open, setOpen] = React.useState(false);
   const [state, dispatch] = useGlobalState();
@@ -182,10 +103,10 @@ export const FullScreenDialog = ({ icon }) => {
           <Paper elevation={3} sx={{ padding: 2, flexGrow: 1, overflow: 'auto' }}>
             <List>
               {state.cart.map((item, index) => (
-                <React.Fragment key={item._id}>
+                <React.Fragment key={item.id}>
                   <ListItem
                     secondaryAction={
-                      <IconButton edge="end" aria-label="delete" onClick={() => handleRemoveFromCart(item._id)}>
+                      <IconButton edge="end" aria-label="delete" onClick={()=>handleRemoveFromCart(item.id)}>
                         <DeleteIcon />
                       </IconButton>
                     }
