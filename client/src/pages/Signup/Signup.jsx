36b8f603a9606adf478599edd2b5ddc19cf4e1cb
So-Emily <esoriano110@gmail.com>
2024-10-08 17:25:24 -0700

    fixed login + signup background using s3 bucket

--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -1,40 +1,43 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../../utils/mutation';
-import Auth from '../../utils/auth';
-import { Link } from 'react-router-dom';
-import { TextField, Button, Typography, Container, Box } from '@mui/material';
+// client/src/pages/Signup/Signup.jsx
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../../utils/mutation";
+import Auth from "../../utils/auth";
+import { Link } from "react-router-dom";
+import { TextField, Button, Typography, Container, Box, Alert } from '@mui/material';
 import '../../assets/styles/AuthForm.css';
 
 const Signup = () => {
-    const [formState, setFormState] = useState({ firstName: '', lastName: '', email: '', password: '' });
+    const [formState, setFormState] = useState({ firstName: "", lastName: "", email: "", password: "" });
     const [errors, setErrors] = useState({});
     const [addUser, { error }] = useMutation(ADD_USER);
+    const [errorMessage, setErrorMessage] = useState("");
 
     const handleChange = (event) => {
         const { name, value } = event.target;
         setFormState({
-            ...formState,
-            [name]: value,
+        ...formState,
+        [name]: value,
         });
 
         // Basic email validation
         if (name === 'email' && !/\S+@\S+\.\S+/.test(value)) {
-            setErrors((prevErrors) => ({ ...prevErrors, email: 'Invalid email address' }));
+        setErrors((prevErrors) => ({ ...prevErrors, email: 'Invalid email address' }));
         } else {
-            setErrors((prevErrors) => ({ ...prevErrors, [name]: '' }));
+        setErrors((prevErrors) => ({ ...prevErrors, [name]: '' }));
         }
     };
 
     const handleFormSubmit = async (event) => {
         event.preventDefault();
         try {
-            const { data } = await addUser({
-                variables: { ...formState },
-            });
-            Auth.login(data.addUser.token);
+        const { data } = await addUser({
+            variables: { ...formState },
+        });
+        Auth.login(data.addUser.token);
         } catch (e) {
-            console.error(e);
+        console.error(e);
+        setErrorMessage("Error signing up. Please try again.");
         }
     };
 
@@ -42,77 +45,65 @@ const Signup = () => {
         <Container className="auth-container">
             <br></br>
             <video autoPlay loop muted className="background-video">
-                <source src="/src/assets/videos/login-background.mp4" type="video/mp4" />
+                <source src="https://coding-videos-bucket.s3.us-east-2.amazonaws.com/login-background.mp4" type="video/mp4" />
             </video>
             <Box className="auth-form" component="form" onSubmit={handleFormSubmit} noValidate>
                 <Typography variant="h4" component="h2" gutterBottom>
-                    Signup
+                Signup
                 </Typography>
                 <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="firstName"
-                    label="First Name"
-                    name="firstName"
-                    autoComplete="fname"
-                    autoFocus
-                    value={formState.firstName}
-                    onChange={handleChange}
-                    error={!!errors.firstName}
-                    helperText={errors.firstName}
+                margin="normal"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                name="firstName"
+                autoComplete="firstName"
+                autoFocus
+                onChange={handleChange}
                 />
                 <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="lastName"
-                    label="Last Name"
-                    name="lastName"
-                    autoComplete="lname"
-                    value={formState.lastName}
-                    onChange={handleChange}
-                    error={!!errors.lastName}
-                    helperText={errors.lastName}
+                margin="normal"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lastName"
+                onChange={handleChange}
                 />
                 <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    value={formState.email}
-                    onChange={handleChange}
-                    error={!!errors.email}
-                    helperText={errors.email}
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                onChange={handleChange}
                 />
                 <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                    value={formState.password}
-                    onChange={handleChange}
-                    error={!!errors.password}
-                    helperText={errors.password}
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={handleChange}
                 />
                 <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
                 >
-                    Sign Up
+                Sign Up
                 </Button>
-                {error && <Typography color="error">{error.message}</Typography>}
+                {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
                 <Link to="/login">
-                    Already have an account? Sign in
+                Already have an account? Login
                 </Link>
             </Box>
         </Container>
